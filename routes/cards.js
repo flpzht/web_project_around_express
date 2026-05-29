93e1c2c55fa04fff689b0e47e6ff268300accb8b
flpzht <flpcsb@hotmail.com>
2026-05-29 14:49:11 -0300

    feat: criacao do router cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,20 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+const router = require('express').Router();
+const { getCards, createCard, deleteCard } = require('../controllers/cards');
 
-const cardsRouter = require('express').Router();
+router.get('/', getCards);
+router.post('/', createCard);
+router.delete('/:id', deleteCard);
 
-const cardsFilePath = path.join(__dirname, '../data/cards.json');
-
-cardsRouter.get('/', (req, res) => {
-  fs.readFile(cardsFilePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.error('Error reading cards data:', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-    const cards = JSON.parse(data);
-    res.json(cards);
-  });
-});
-
-module.exports = cardsRouter;
+module.exports = router;
