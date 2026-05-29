79d6b3518e03fb1792afd3cefe20bd310316540a
flpzht <flpcsb@hotmail.com>
2026-05-29 15:56:00 -0300

    feat: manipulacao de erros no controller users

--- controllers/users.js
+++ controllers/users.js
@@ -8,13 +8,32 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.id)
+    .onFail(() => {
+      const error = new Error('User not found');
+      error.statusCode = 404;
+      throw error;
+    })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Invalid user ID' });
+      }
+      if (err.statusCode === 404) {
+        return res.status(404).send({ message: err.message });
+      }
+
+      return res.status(500).send({ message: 'Internal server error' });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Invalid user data' });
+      }
+      return res.status(500).send({ message: 'Internal server error' });
+    });
 };
