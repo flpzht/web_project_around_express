fae563233cd5e0eed18c27ff84a19037dd911f15
flpzht <flpcsb@hotmail.com>
2026-05-29 16:20:41 -0300

    feat: adicao de like e dislike cards no controller cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -28,7 +28,47 @@ module.exports.deleteCard = (req, res) => {
       if (!card) {
         return res.status(NOT_FOUND).send({ message: 'Card not found' });
       }
-      res.send({ data: card });
+      return res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(BAD_REQUEST).send({ message: 'Invalid card ID' });
+      }
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' });
+    });
+};
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.id,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(NOT_FOUND).send({ message: 'Card not found' });
+      }
+      return res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(BAD_REQUEST).send({ message: 'Invalid card ID' });
+      }
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' });
+    });
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.id,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(NOT_FOUND).send({ message: 'Card not found' });
+      }
+      return res.send({ data: card });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
