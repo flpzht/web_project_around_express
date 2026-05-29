6258044c0635a67b5a67baa5870f4d8ca4fafcf3
flpzht <flpcsb@hotmail.com>
2026-05-29 10:59:11 -0300

    feat: criacao do schema model users

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.json());
 
