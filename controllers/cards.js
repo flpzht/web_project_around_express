a9a5e3d2ba1379b1df1c25a03b8ea5a4fb20f348
flpzht <flpcsb@hotmail.com>
2026-05-29 14:47:52 -0300

    feat: criacao do controller cards

---
+++ controllers/cards.js
@@ -0,0 +1,25 @@
+const Card = require('../models/cards');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.id)
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Card not found' });
+      }
+      res.send({ data: card });
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
