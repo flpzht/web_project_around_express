180ef616906d8ecba11867cfcad710f440fafb54
flpzht <flpcsb@hotmail.com>
2026-05-29 16:22:54 -0300

    feat: adicao de update perfil e avatar no controller users

--- controllers/users.js
+++ controllers/users.js
@@ -41,3 +41,51 @@ module.exports.createUser = (req, res) => {
       return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' });
     });
 };
+
+module.exports.updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .orFail(() => {
+      const error = new Error('User not found');
+      error.statusCode = NOT_FOUND;
+      throw error;
+    })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(BAD_REQUEST).send({ message: 'Invalid user data' });
+      }
+      if (err.statusCode === NOT_FOUND) {
+        return res.status(NOT_FOUND).send({ message: err.message });
+      }
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' });
+    });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true },
+  )
+    .orFail(() => {
+      const error = new Error('User not found');
+      error.statusCode = NOT_FOUND;
+      throw error;
+    })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(BAD_REQUEST).send({ message: 'Invalid avatar URL' });
+      }
+      if (err.statusCode === NOT_FOUND) {
+        return res.status(NOT_FOUND).send({ message: err.message });
+      }
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Internal server error' });
+    });
+};
